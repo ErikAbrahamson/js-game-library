05cd8564812c11f4d50155926e64980f08974454
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-08-10 11:53:23 -0600

    Add form

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
 $(document).on('ready', function() {
-  function Game(title, genre) {
-    this.title = title || 'no title';
-    this.genre = genre || 'no genre';
+  var lib = $('#library'), addBtn = $('#add');
+  var $title = $('#title').val();
+  var $genre = $('#genre').val();
+  var $pic = $('#image').val();
+  function Game(title, genre, imgURL) {
+    this.title = $title || 'no title';
+    this.genre = $genre || 'no genre';
+    this.imgURL = $pic || null;
   }
   function GameLibrary(title, games) {
     this.title = title || 'no title';
     this.games = [];
   }
-  Game.prototype.render = function(event) {
-    event.preventDefault();
-    var span = $('body').append('span')
-      .addClass('game');
-    span.text(this.title + this.genre);
+  Game.prototype.render = function() {
+    addBtn.on('click', function(event) {
+      lib.append('div')
+        .text(this.title + ', ' + this.genre)
+        .addClass('game');
+    });
   };
   GameLibrary.prototype.render = function(event) {
     event.preventDefault();
-    var span = $('body').append('span')
+    span.append
       .addClass('library');
     span.text(this.title + this.genre);
   };
