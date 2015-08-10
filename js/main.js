3d81ec3348cdf64bea45d404059fef232810da63
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-08-10 10:52:34 -0600

    add game and lib classes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
-// add scripts
-
 $(document).on('ready', function() {
-  console.log('sanity check!');
+  function Game(title, genre) {
+    this.title = title || 'no title';
+    this.genre = genre || 'no genre';
+  }
+  function GameLibrary(title, games) {
+    this.title = title || 'no title';
+    this.games = [];
+  }
+  Game.prototype.render = function(event) {
+    event.preventDefault();
+    var span = $('body').append('span')
+      .addClass('game');
+    span.text(this.title + this.genre);
+  };
+  GameLibrary.prototype.render = function(event) {
+    event.preventDefault();
+    var span = $('body').append('span')
+      .addClass('library');
+    span.text(this.title + this.genre);
+  };
+
+
 });
