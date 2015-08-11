6bd697bcaaccfd7b5af6055dc4cc6e976dbc636d
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-08-11 12:57:23 -0600

    styling

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@ function Game(title, genre, image) {
   this.image = image || null;
 }
 
-function GameLibrary(title) {
-  this.title = title;
+function GameLibrary() {
   this.games = [];
 }
-
+GameLibrary.prototype.addGame = function(game) {
+  this.games.push(game);
+};
 Game.prototype.render = function() {
   var game = $('<div>')
     .append('<strong>' + this.title + '</strong>')
@@ -21,11 +22,20 @@ Game.prototype.render = function() {
   return this.$element;
 };
 
+var library = new GameLibrary();
+
 $('#add').on('click', function(event) {
-  console.log('Clicked!');
   event.preventDefault();
   var game = new Game($('#title').val(),$('#genre').val(),$('#image').val());
+  library.addGame(game);
   game.render();
+  for (var i = $('option').length; i > 0; i--) {
+    if ($('option').eq(i).val() !== game.genre) {
+    $('#select-genre').append('<option value="' + game.genre + '">' + game.genre + '</option>');
+    } else {
+      console.log('Genre already exists');
+    }
+  }
   $('input').val(null);
 });
 
@@ -39,3 +49,4 @@ $('#remove').on('click', function(event) {
     }
   }
 });
+
